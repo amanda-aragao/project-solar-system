c8423ec94fe947262857e260d9be2513ff1d9eaa
Amanda Aragão <amandaaragao72@gmail.com>
2023-03-01 17:38:46 -0300

    Realizando requisito 7

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import PlanetCard from './PlanetCard';
 import Title from './Title';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
+    const planetList = (planet) => {
+      const { name, image } = planet;
+      return (
+        <PlanetCard key={ name } planetName={ name } planetImage={ image } />
+      );
+    };
+
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {/* para cada elemento do meu array de planetas recebera minha função que retorna um componente(card) com props name e image. */}
+        <div>
+          {planets.map(planetList)}
+        </div>
       </div>
     );
   }
